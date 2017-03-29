0fd128a26983bda93381c45643313a9dfa329afa
Aapeli <aapeli.haanpuu@unifina.com>
2017-03-29 18:39:08 +0300

    dbconfig

--- database/config.js
+++ database/config.js
@@ -2,8 +2,8 @@
 // Define mysql properties here (or change dialect to something else)
 module.exports = {
     dialect: 'mysql',
-    host: 'dev.streamr',
-    database: 'aapelimoi',
-    user: 'root',
-    password: 'Trez2tuV'
+    host: undefined,
+    database: undefined,
+    user: undefined,
+    password: undefined
 }
--- database/connection.js
+++ database/connection.js
@@ -1,6 +1,10 @@
 const Sequelize = require('sequelize')
 const dbConfig = require('./config')
 
+if (!dbConfig.host) {
+    throw new Error("Please specify database credentials in 'database/config.js'!")
+}
+
 const sequelize = new Sequelize(dbConfig.database, dbConfig.user, dbConfig.password, {
     host: dbConfig.host,
     port: dbConfig.port,
